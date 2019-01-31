a0388bafb3a0d0c31c6a6d38a383c89b52445c7c
Dakota Metz <metzcorp@aol.com>
2019-01-31 12:38:15 -0500

    Created Sample and ensured it worked.

---
+++ WpfViewSample/Classes/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfViewSample.Classes
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Char MI { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+
+        public List<Person> GetPeople() //Generates a generic list of people.
+        {
+            List<Person> PeopleList = new List<Person>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                PeopleList.Add(new Person
+                {
+                    FirstName = $"First Name {i}",
+                    LastName = $"Last Name {i}",
+                    MI = 'A',
+                    Address = $"{i} {i + 1} {i + 2} Street City State",
+                    Number = i
+                });
+            }
+            return PeopleList;
+        }
+    }
+}
--- WpfViewSample/MainWindow.xaml.cs
+++ WpfViewSample/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfViewSample.Classes;
 
 namespace WpfViewSample
 {
@@ -20,9 +21,48 @@ namespace WpfViewSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        Person people = new Person();
+        Person Person = new Person();
         public MainWindow()
         {
             InitializeComponent();
+            ListViewTemplate.ItemsSource = people.GetPeople();
+            ListViewNoTemplate.ItemsSource = people.GetPeople();
+            DataGridView.ItemsSource = people.GetPeople();
+            ComboxView.ItemsSource = people.GetPeople();
+        }
+
+        private void ListViewTemplate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person = (Person)ListViewTemplate.SelectedItem;
+            ShowPerson();
+        }
+
+        private void ListViewNoTemplate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person = (Person)ListViewNoTemplate.SelectedItem;
+            ShowPerson();
+        }
+
+        private void DataGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person = (Person)DataGridView.SelectedItem;
+            ShowPerson();
+        }
+
+        private void ComboxView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person = (Person)ComboxView.SelectedItem;
+            ShowPerson();
+        }
+
+        //Accessing the selected Item
+        private void ShowPerson()
+        {
+            FirstNameText.Text = Person.FirstName;
+            LastNameText.Text = Person.LastName;
+            AddressText.Text = Person.Address;
+            NumberText.Text = Person.Number.ToString();
         }
     }
 }
